16007e1c7bbbb4abe00c9006b9d033b3a6cbd788
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-05-11 02:42:06 +0000

    updated

--- payroll/models.py
+++ payroll/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class BaseModel(models.Model):
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+  created_at = models.DateTimeField(default=timezone.now
+  updated_at = models.DateTimeField(default=timezone.now)
 
   class Meta:
     abstract = True
